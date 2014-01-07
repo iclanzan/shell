d9dfebc7ecc5f1931d45af002c7f270d2301209f
Sorin Iclanzan <sorin@iclanzan.com>
2014-01-06 23:47:09 -0800

    Added missing alias keyword.

--- .profile.d/appearance.sh
+++ .profile.d/appearance.sh
@@ -6,7 +6,7 @@ if [[ $os_name == "Darwin" ]] ; then
   alias ls='ls -G'
 elif [[ $os_name == "Linux" ]]; then
   export LS_COLORS="di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=0;46:cd=0;43:su=0;41:sg=0;45:tw=0;42:ow=0;47:"
-  ls='ls --color=tty'
+  alias ls='ls --color=tty'
 fi
 
 # Color grep results
